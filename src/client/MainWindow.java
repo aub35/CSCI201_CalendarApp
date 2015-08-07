e5fa00ca407ceefa44f45b57c53081630384408a
Jingyu Wang <wangjingyu0422@gmail.com>
2015-08-07 12:32:45 -0700

    add centerPanel for mainGUI

--- src/client/MainWindow.java
+++ src/client/MainWindow.java
@@ -1,6 +1,8 @@
 package client;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,12 +10,19 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class MainWindow extends JFrame {
 	JButton addEventButton;
-	JPanel rightPanel;
+	JPanel centerPanel, rightPanel, dayPanel;
 	JDialog EventWindow;
+	JScrollPane jsp;
+	GridBagConstraints gbc;
+	JLabel [] hourLabels;
+	JLabel [] eventLabels;
 	
 	public MainWindow() {
 		createGUI();
@@ -24,6 +33,51 @@ public class MainWindow extends JFrame {
 	}
 	
 	public void createGUI(){
+		
+		//centerPanel
+		centerPanel = new JPanel();
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+		
+		//Panel for Calendar
+		//Daily Mode
+		dayPanel = new JPanel();
+		dayPanel.setLayout(new GridBagLayout());
+		gbc = new GridBagConstraints();
+		
+		hourLabels = new JLabel[24];
+		eventLabels = new JLabel[24];
+		
+		for (int i=0; i<24; i++){
+			JLabel label = new JLabel(i+":00");
+			gbc.gridx = 0;
+			gbc.gridy = i;
+			hourLabels[i] = label;
+			dayPanel.add(label, gbc);
+		}
+		
+		for (int i=0; i<24; i++){
+			JLabel label = new JLabel("...");
+			gbc.gridx = 2;
+			gbc.gridy = i;
+			gbc.gridwidth = 5;
+			eventLabels[i] = label;
+			dayPanel.add(label, gbc);
+		}
+		
+		jsp = new JScrollPane(dayPanel);
+		jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		centerPanel.add(jsp);
+		
+		JTextArea eventTextArea = new JTextArea();
+		eventTextArea.setRows(10);
+		centerPanel.add(eventTextArea);
+		
+		add(centerPanel);
+
+		
+		
+		// rightPanel
 		rightPanel = new JPanel();
 		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
 		
